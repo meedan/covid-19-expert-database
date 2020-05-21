e617cc58196716230ffebd6a80c2a3ac4aeb1a3b
infojunkie <karim.ratib@gmail.com>
2020-05-21 13:02:25 -0700

    Trim keys

--- _scripts/csv2json.rb
+++ _scripts/csv2json.rb
@@ -8,7 +8,9 @@ require 'csv'
 require 'json'
 
 lines = CSV.open(ARGV[0], 'r:bom|utf-8').readlines
-keys = lines.delete lines.first
+keys = lines.delete(lines.first).map do |key|
+  key.strip
+end
 File.open(ARGV[1], 'w') do |f|
   data = lines.select { |values| values.any? }.map do |values|
     Hash[keys.zip(values)]
