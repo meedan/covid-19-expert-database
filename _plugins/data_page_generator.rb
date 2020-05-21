20dfd2b7851f062135b3546d7b24a2dcdb4a5868
infojunkie <karim.ratib@gmail.com>
2020-05-21 14:07:25 -0700

    Link questions to each other, initial commit

--- _plugins/data_page_generator.rb
+++ _plugins/data_page_generator.rb
@@ -17,24 +17,25 @@ module Jekyll
   ).downcase.strip.gsub(' ', '-').gsub(/[^\w.-]/, '')
       end
     end
-  
+
     # this class is used to tell Jekyll to generate a page
     class DataPage < Page
       include Sanitizer
-  
+
       # - site and base are copied from other plugins: to be honest, I am not sure what they do
       #
       # - `index_files` specifies if we want to generate named folders (true) or not (false)
       # - `dir` is the default output directory
       # - `data` is the data defined in `_data.yml` of the record for which we are generating a page
+      # - `data_index` is the index of the record
       # - `name` is the key in `data` which determines the output filename
       # - `name_expr` is an expression for generating the output filename
       # - `template` is the name of the template for generating the page
       # - `extension` is the extension for the generated file
-      def initialize(site, base, index_files, dir, data, name, name_expr, template, extension)
+      def initialize(site, base, index_files, dir, data, data_index, name, name_expr, template, extension)
         @site = site
         @base = base
-  
+
         # @dir is the directory where we want to output the page
         # @name is the name of the page to generate
         # @name_expr is an expression for generating the name of the page
@@ -43,6 +44,7 @@ module Jekyll
         # want to generate named folders or not
         if name_expr
           record = data
+          record_index = data_index
           raw_filename = eval(name_expr)
           if raw_filename == nil
             puts "error (datapage_gen). name_expr '#{name_expr}' generated an empty value in record #{data}"
@@ -55,12 +57,12 @@ module Jekyll
             return
           end
         end
-  
+
         filename = sanitize_filename(raw_filename).to_s
-  
+
         @dir = dir + (index_files ? "/" + filename + "/" : "")
         @name = (index_files ? "index" : filename) + "." + extension.to_s
-  
+
         self.process(@name)
         self.read_yaml(File.join(base, '_layouts'), template + ".html")
         self.data['title'] = raw_filename
@@ -83,28 +85,28 @@ module Jekyll
 
         # add all the information defined in _data for the current record to the
         # current page (so that we can access it with liquid tags)
-  
+
         if data.key?('name')
           data['_name'] = data['name']
         end
-  
+
         self.data.merge!(data)
       end
     end
-  
+
     class DataPagesGenerator < Generator
       safe true
-  
+
       # generate loops over _config.yml/page_gen invoking the DataPage
       # constructor for each record for which we want to generate a page
-  
+
       def generate(site)
         # page_gen_dirs determines whether we want to generate index pages
         # (name/index.html) or standard files (name.html). This information
         # is passed to the DataPage constructor, which sets the @dir variable
         # as required by this directive
         index_files = site.config['page_gen-dirs'] == true
-  
+
         # data contains the specification of the data for which we want to generate
         # the pages (look at the README file for its specification)
         data = site.config['page_gen']
@@ -116,7 +118,7 @@ module Jekyll
             name_expr = data_spec['name_expr']
             dir = data_spec['dir'] || data_spec['data']
             extension = data_spec['extension'] || "html"
-  
+
             if site.layouts.key? template
               # records is the list of records defined in _data.yml
               # for which we want to generate different pages
@@ -128,15 +130,15 @@ module Jekyll
                   records = records[level]
                 end
               end
-  
+
               # apply filtering conditions:
               # - filter requires the name of a boolean field
               # - filter_condition evals a ruby expression
               records = records.select { |r| r[data_spec['filter']] } if data_spec['filter']
               records = records.select { |record| eval(data_spec['filter_condition']) } if data_spec['filter_condition']
-  
-              records.each do |record|
-                site.pages << DataPage.new(site, site.source, index_files_for_this_data, dir, record, name, name_expr, template, extension)
+
+              records.each_with_index do |record, index|
+                site.pages << DataPage.new(site, site.source, index_files_for_this_data, dir, record, index, name, name_expr, template, extension)
               end
             else
               puts "error (datapage_gen). could not find template #{template}" if not site.layouts.key? template
@@ -145,10 +147,10 @@ module Jekyll
         end
       end
     end
-  
+
     module DataPageLinkGenerator
       include Sanitizer
-  
+
       # use it like this: {{input | datapage_url: dir}}
       # to generate a link to a data_page.
       #
@@ -165,7 +167,7 @@ module Jekyll
         "#{dir}/#{sanitize_filename(input)}#{extension}"
       end
     end
-  
+
   end
-  
+
   Liquid::Template.register_filter(Jekyll::DataPageLinkGenerator)
