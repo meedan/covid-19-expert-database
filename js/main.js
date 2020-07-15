234fcc7e1e0dea3fcb9186d061a70c9a40be3ef7
Abdul Rehman Khawar <designbysoil@gmail.com>
2020-07-14 17:43:19 -0700

    Updating after airtable column renames.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 (function() {
     $(document).ready(function() {
 
-        tippy('a.glossary-term', {
-			animation: 'shift-away',
-            theme: 'term',
-            allowHTML: true,
-            interactive: true,
-            interactiveBorder: 30,
-            arrow: false,
-		});
+        //
 
     });
 })();
