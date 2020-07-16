9046153a055cf1c950c0cf16c5f91a17c1c2a43f
Abdul Rehman Khawar <designbysoil@gmail.com>
2020-07-15 18:35:24 -0700

    Updating algolia hooks for category and topics.:

--- _plugins/algolia_hooks.rb
+++ _plugins/algolia_hooks.rb
@@ -3,10 +3,10 @@ module Jekyll
     module Hooks
       def self.before_indexing_each(record, node, context)
         if record[:Category]
-          record[:categories] = Jekyll::Utils::split_better(record[:Category])
+          record[:categories] = record[:Category]
         end
         if record[:Topics]
-          record[:topics] = Jekyll::Utils::split_better(record[:Topics])
+          record[:topics] = record[:Topics]
         end
 
         record
