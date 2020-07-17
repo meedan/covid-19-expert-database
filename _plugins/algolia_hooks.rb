df6748bea79186734b04f0031135c9d07ee30615
Abdul Rehman Khawar <designbysoil@gmail.com>
2020-07-17 16:39:06 -0700

    Further work on search.

--- _plugins/algolia_hooks.rb
+++ _plugins/algolia_hooks.rb
@@ -4,13 +4,27 @@ module Jekyll
       def self.before_indexing_each(record, node, context)
         if record[:Category]
           record[:categories] = record[:Category]
+          record[:content_type] = 'question'
         end
         if record[:Topics]
           record[:topics] = record[:Topics]
         end
+        if record[:Term]
+          record[:content_type] = 'glossary'
+        end
 
         record
       end
+
+      def self.should_be_excluded?(filepath)
+        # Do not index 
+        return true if filepath =~ %r{/about/}
+        return true if filepath =~ %r{ask-a-question}
+        return true if filepath =~ %r{thanks}
+        return true if filepath =~ %r{meet-our-experts}
+        false
+      end
+      
     end
   end
 end
