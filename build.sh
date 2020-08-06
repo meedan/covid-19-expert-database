834e5e2d5cebdec5ae7a3afa550b43084f6059e6
Abdul Rehman Khawar <designbysoil@gmail.com>
2020-08-06 14:40:25 -0700

    Adding jekyll contentful to build file.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ALGOLIA_INDEX_NAME=$ALGOLIA_INDEX_NAME_QUESTIONS bundle exec jekyll algolia
-ALGOLIA_INDEX_NAME=$ALGOLIA_INDEX_NAME_GLOSSARY bundle exec jekyll algolia --config _config-glossary.yml
+jekyll contentful
+ALGOLIA_INDEX_NAME=$ALGOLIA_INDEX_NAME_QUESTIONS jekyll algolia
+ALGOLIA_INDEX_NAME=$ALGOLIA_INDEX_NAME_GLOSSARY jekyll algolia --config _config-glossary.yml
